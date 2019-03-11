b6ddd1b1cad4881b5709b2e039cd25789aa86e33
Jelle van de Haterd <j.vandehaterd@developers.nl>
2019-03-11 13:35:32 +0100

    request_password args with safe instead of vault

--- action_plugins/cyberark_request.py
+++ action_plugins/cyberark_request.py
@@ -58,6 +58,7 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
+        safe = self._templar.template(self._task.args.get('safe', None))
         keywords = self._templar.template(self._task.args.get('keywords', []))
         reason = self._templar.template(self._task.args.get('reason'), fail_on_undefined=True)
         wait = boolean(self._task.args.get('wait', True), strict=False)
@@ -92,7 +93,7 @@ class ActionModule(ActionBase):
                     wait=wait,
                     reason=reason,
                     period=period,
-                    safe=vault
+                    safe=safe
                 )
 
                 if 'failure' in single_result:
